755a791c66742a7c8df3efa9acea8ae4ffff8115
Ariel Conti <arielconti@DESKTOP-IT9C7BQ.localdomain>
2019-12-08 14:19:18 -0200

    feat: restructure components folder

--- src/views/VinCheck.tsx
+++ src/views/VinCheck.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { connect } from "react-redux"
 
-import { VinInput } from "../components/VinInput"
-import { CarInfoPreview } from "../components/CarInfoPreview"
+import { VinInput } from "../components/VinInput/VinInput"
+import { CarInfoPreview } from "../components/CarInfoPreview/CarInfoPreview"
 import { actions, MapState, MapDispatch } from "../store"
 type Props = Pick<RootState, "vin" | "vinCheckResult" | "vinValidationError">
 type Actions = Pick<typeof actions, "setVin" | "checkVin">
@@ -25,7 +25,4 @@ const mapDispatch: MapDispatch<Actions> = dispatch => ({
     checkVin: () => dispatch(actions.checkVin())
 })
 
-export const VinCheckView = connect(
-    mapState,
-    mapDispatch
-)(VinCheck)
+export const VinCheckView = connect(mapState, mapDispatch)(VinCheck)
